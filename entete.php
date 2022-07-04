0fb388c82fbdea78911004c3608c6bfc2d269293
Allister K <allister.kohn@gmail.com>
2022-07-04 23:42:22 +0200

    modifs entete

--- entete.php
+++ entete.php
@@ -43,7 +43,7 @@
     </div>
 
     <center>
-        <iframe src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html" class="m_rolex_clock"
+        <iframe class="container" src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html" class="m_rolex_clock"
                 SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
     </center>
 </div>
