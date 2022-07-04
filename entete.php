2ef8b6a7be2480762a269b1c72d6f214fefc28ee
Allister K <allister.kohn@gmail.com>
2022-07-04 23:43:11 +0200

    modifs entete

--- entete.php
+++ entete.php
@@ -1,9 +1,11 @@
+<div class="container">
 <center>
     <br><br>
     <a href="index.php">
         <img src="Images/RFTM2.png" alt="Logo" title="Logo RFTM" width="230"/>
     </a>
 </center>
+</div>
 
 <div class="container"><br><br>
     <div id="demo" class="carousel slide carousel-fade" data-ride="carousel">
@@ -49,18 +51,9 @@
 </div>
 
 
-
 <!--
 <center>
     <br/><br/>
     <img src="Images/banniere.jpeg" alt="banniere" title="Bannière RFTM" width="1000"/>
 </center>
 -->
-
-
-<nav>
-    <center>
-        <iframe src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html" class="m_rolex_clock"
-                SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
-    </center>
-</nav>
