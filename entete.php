02b1217360fdeb92980ce7142fbdb08ef892c7c8
Allister K <allister.kohn@gmail.com>
2022-07-04 23:40:54 +0200

    modifs entete

--- entete.php
+++ entete.php
@@ -1,19 +1,61 @@
 <center>
-    <br/> <br/>
+    <br><br>
     <a href="index.php">
         <img src="Images/RFTM2.png" alt="Logo" title="Logo RFTM" width="230"/>
     </a>
 </center>
 
+<div class="container"><br><br>
+    <div id="demo" class="carousel slide carousel-fade" data-ride="carousel">
+        <!-- Indicateurs -->
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+            <li data-target="#demo" data-slide-to="3"></li>
+        </ul>
+
+        <!-- Carrousel -->
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-interval="4000">
+                <img src="Images/banniere1.png" alt="Carrousel slide 1" class="d-block w-100">
+            </div>
+            <div class="carousel-item" data-interval="4000">
+                <img src="Images/banniere2.png" alt="Carrousel slide 2" class="d-block w-100">
+            </div>
+            <div class="carousel-item" data-interval="4000">
+                <img src="Images/banniere3.png" alt="Carrousel slide 3" class="d-block w-100">
+            </div>
+            <div class="carousel-item" data-interval="4000">
+                <img src="Images/banniere4.png" alt="Carrousel slide 4" class="d-block w-100">
+            </div>
+        </div>
+
+        <!-- Contrôles -->
+        <a class="carousel-control-prev" href="#demo" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Précédent</span>
+        </a>
+        <a class="carousel-control-next" href="#demo" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Suivant</span>
+        </a>
+    </div>
+</div>
+
+
+
+<!--
 <center>
     <br/><br/>
     <img src="Images/banniere.jpeg" alt="banniere" title="Bannière RFTM" width="1000"/>
 </center>
+-->
 
 
 <nav>
     <center>
         <iframe src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html" class="m_rolex_clock"
-                SCROLLING=NO frameborder=NO width="1001px" height="75px"></iframe>
+                SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
     </center>
 </nav>
