3eebd67f71b1b32210a0c5c5fd2a8438f0615ce9
Allister K <allister.kohn@gmail.com>
2022-07-06 12:16:50 +0200

    div container rolex

--- entete.php
+++ entete.php
@@ -46,8 +46,10 @@
 </div>
 
 <div>
-    <iframe class="container" src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html"
-            class="m_rolex_clock" SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
+    <center>
+        <iframe class="container" src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html"
+                class="m_rolex_clock" SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
+    </center>
 </div>
 
 
