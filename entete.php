140ddb9fd39f0f8ae89679a96b775a34c88a64bf
Allister K <allister.kohn@gmail.com>
2022-07-06 12:15:14 +0200

    changement icone apple

--- entete.php
+++ entete.php
@@ -44,7 +44,7 @@
         </a>
     </div>
 
-    <div>
+    <div class="container">
         <iframe src="https://static.rolex.com/clocks/2020/federer_mobile_HTML_180x75/rolex.html"
                 class="m_rolex_clock" SCROLLING=NO frameborder=NO width="1110px" height="75px"></iframe>
     </div>
