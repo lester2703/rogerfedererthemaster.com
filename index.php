8f4ac12746127c4cab59beff602a977d3c0dfb9a
Allister K <allister.kohn@gmail.com>
2022-07-06 12:07:23 +0200

    modifs ajout mail dans bdd

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>RFTM Website</title>
     <meta name="description" content="" />
     <link rel="icon" type="image/png" sizes="16x16" href="Images/Logo RFTM Blanc PNG.png">
+    <link rel="apple-touch-icon" href="Images/Logo RFTM Blanc PNG.png">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossorigin="anonymous"></script>
